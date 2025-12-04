761a8a4c4a544962943e14a9e6b659b4828f7693
AdarshNetha <abhiadarshnetha849@gmail.com>
2025-12-04 22:26:13 +0530

    added mapings

--- src/main/java/com/aan/LetsRide/controller/DriverController.java
+++ src/main/java/com/aan/LetsRide/controller/DriverController.java
@@ -2,6 +2,10 @@ package com.aan.LetsRide.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.aan.LetsRide.service.DriverService;
 
@@ -10,6 +14,18 @@ public class DriverController {
 @Autowired
 private DriverService driverservice;
 
+@PostMapping("/driver/registerDriver")
+
+
+
+@DeleteMapping("/driver/deleteDriver")
+
+
+@GetMapping("/driver/findDribver")
+
+
+
+@PutMapping("/drivr/upadateCurrentVechileLocation")
 
 
 
