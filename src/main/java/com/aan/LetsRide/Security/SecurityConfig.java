6625a337006612dcaff3e3403296663631f5af36
vamshi reddy <vamshireddy0228@gmail.com>
2026-01-06 19:18:01 +0530

    complete

--- src/main/java/com/aan/LetsRide/Security/SecurityConfig.java
+++ src/main/java/com/aan/LetsRide/Security/SecurityConfig.java
@@ -35,15 +35,8 @@ public class SecurityConfig {
 
         http
 
-            
-
             .cors(cors -> cors.configurationSource(corsConfigurationSource())) // <-- enable CORS
-<<<<<<< HEAD
 
-=======
-            
-            
->>>>>>> c4f5550a4d237b96ce5088cdeb8ed47a35de09e2
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
